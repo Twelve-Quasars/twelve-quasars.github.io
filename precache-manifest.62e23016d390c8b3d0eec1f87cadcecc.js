36ed9ad1fee1faec58719ed69c439859437b7c1c
Colin Brayton <gringolalia@gmail.com>
2020-09-15 10:37:01 -0300

    deploy

---
+++ precache-manifest.62e23016d390c8b3d0eec1f87cadcecc.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e5743429e61dd06134a",
+    "url": "/css/about.eccec030.css"
+  },
+  {
+    "revision": "82eb619961f63ce41a09",
+    "url": "/css/app.5af624d8.css"
+  },
+  {
+    "revision": "09876f9eeee641398cbd",
+    "url": "/css/chunk-vendors.4a579a34.css"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/fonts/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/fonts/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/fonts/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/fonts/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/fonts/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/fonts/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "463cfa6bda38af7123b1770d4982ad3c",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.463cfa6b.woff"
+  },
+  {
+    "revision": "8ff0ce021911cf276edc22fb8ca59ec7",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.8ff0ce02.woff2"
+  },
+  {
+    "revision": "b4917be25082eb793b5363f2fdb5f282",
+    "url": "/fonts/materialdesignicons-webfont.b4917be2.woff"
+  },
+  {
+    "revision": "d0066537ab6a4c6f8285a5aeb3ba5f09",
+    "url": "/fonts/materialdesignicons-webfont.d0066537.woff2"
+  },
+  {
+    "revision": "fd5d35888a6a1517bd44c0023f228344",
+    "url": "/img/brooklyn-bridge-warship.fd5d3588.jpg"
+  },
+  {
+    "revision": "7226a46e7f28a60e289da005060e2db6",
+    "url": "/img/enigma.7226a46e.jpg"
+  },
+  {
+    "revision": "8c4120b4e88276badbc808e518572fad",
+    "url": "/img/logo.8c4120b4.png"
+  },
+  {
+    "revision": "c1962138b9c4e619113effef2e519536",
+    "url": "/img/machiine-one.c1962138.jpg"
+  },
+  {
+    "revision": "dcade5d493c0c508172f0dba96f3716f",
+    "url": "/img/nyc-tenement.dcade5d4.jpg"
+  },
+  {
+    "revision": "ee42ce781439fc5251447ce3ad822372",
+    "url": "/img/vintage-test-one.ee42ce78.jpg"
+  },
+  {
+    "revision": "6222864a420561bd19bed5afbf111df2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e5743429e61dd06134a",
+    "url": "/js/about.2c0f309e.js"
+  },
+  {
+    "revision": "82eb619961f63ce41a09",
+    "url": "/js/app.e1c2f379.js"
+  },
+  {
+    "revision": "09876f9eeee641398cbd",
+    "url": "/js/chunk-vendors.e6579027.js"
+  },
+  {
+    "revision": "2907f3e29c15a63a690a750c7787e9fc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
